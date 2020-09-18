b66057fc6cc8d4d5c52972448a5f542fa340054f
exzeshko <exzeshko@gmail.com>
2020-09-18 22:35:02 +0200

    bugs

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ export default class ContactForm extends React.Component {
   initClient = () => { 
     gapi.client.init({
       'apiKey': API_KEY,
-      'clientId': CLIENT_ID,
       'scope': SCOPE,
       'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(()=> {
