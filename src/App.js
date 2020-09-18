43e4240d30e9da79677acf5db0fbea0ba24a5460
exzeshko <exzeshko@gmail.com>
2020-09-18 19:40:48 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -1,26 +1,258 @@
+import { gapi } from 'gapi-script';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CSVReader } from 'react-papaparse'
+import { mainData } from './data'
+import { Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const buttonRef = React.createRef()
+const SPREADSHEET_ID = '1sad08ntggQL4M4wihOX16ccc6XDqfLgfOeymVVGiX44'; //from the URL of your blank Google Sheet
+const CLIENT_ID = '457989737322-7117pqto0a1skcmbgvle4k405avob8e4.apps.googleusercontent.com'; //from https://console.developers.google.com/apis/credentials
+const API_KEY = 'AIzaSyDX9HSVapiGgzfxo33QH-SMNqmU8CuIOII'; //https://console.developers.google.com/apis/credentials
+const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
+
+
+export default class ContactForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      datas: '',
+      opozdun: '',
+      loading: false,
+      loadingComplete: false,
+      tableReady: false
+    }
+    
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.proverochka = this.proverochka.bind(this)
+  }
+
+  componentDidMount(){ 
+    this.handleClientLoad();   
+  }
+
+  handleClientLoad = () => { 
+    gapi.load('client:auth2', this.initClient);
+  }
+
+  initClient = () => { 
+    gapi.client.init({
+      'apiKey': API_KEY,
+      'clientId': CLIENT_ID,
+      'scope': SCOPE,
+      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(()=> {
+      console.log('ok')
+    });
+  }
+
+  proverochka() {
+    this.setState({loading:false})
+  }
+
+  onFormSubmit() {
+    this.setState({loading: true})
+    let peopleTime = this.state.datas
+    const date1 = this.state.datas[0].data[0]
+    const date2 = this.state.datas[0].data[1]
+    const mySplits1 = date1.split(/[.,: -]/);
+  const mySplits2 = date2.split(/[.,: -]/);
+  var result  = mySplits1.map(function(item) {
+    var number = parseInt(item);
+    return isNaN(number)? item : number;
+  });
+  var result2  = mySplits2.map(function(item) {
+    var number = parseInt(item);
+    return isNaN(number)? item : number;
+  });
+   if(result[0] < result2[0]){
+     result[0]++
+     result[1] = 0
+   }
+  let difference = result2[1] - result[1]
+  let opozdal = []
+   peopleTime.filter(function(item){
+     if(item.data[1] < difference) {
+       opozdal.push(item.data[0])
+     }
+   })
+   this.setState({opozdun: opozdal})
+    let batchUpdateSpreadsheetRequestBody = {
+      requests: [
+        {
+          "insertDimension": {
+            "range": {
+              "sheetId": 0,
+              "dimension": "COLUMNS",
+              "startIndex": 2,
+              "endIndex": 3
+            },
+            "inheritFromBefore": true
+          }
+        }
+      ]
+    }
+    let parameters = { spreadsheetId: SPREADSHEET_ID}
+    let request = gapi.client.sheets.spreadsheets.batchUpdate(parameters, batchUpdateSpreadsheetRequestBody);
+    request.then(function(response) {
+      console.log(response.result);
+    }, function(reason) {
+      console.error('error: ' + reason.result.error.message);
+    });
+  
+    let arr = []
+    let csvData = this.state.datas
+    mainData.forEach(function(item) {
+      csvData.forEach(function(items){
+        if(item.name === items.data[0]){
+            arr.push(item.id+1)
+        }
+      })
+    }
+    )
+    setTimeout(function(){
+    let count = 0
+    let i = -1
+    setInterval(function(){  
+      if (count < arr.length){
+        count ++
+        i ++
+    const params = {
+      spreadsheetId: SPREADSHEET_ID,  
+    };
+    
+    const valueRangeBody = {
+      "valueInputOption": "RAW",
+      "data": [
+      {
+      "majorDimension": "ROWS",
+      "range" : 'Sheet1!C'+ arr[i] + ':C91',
+      "values" : [["1"]]
+      }
+      ]
+    };
+    let request = gapi.client.sheets.spreadsheets.values.batchUpdate(params, valueRangeBody);
+    request.then(function (response) {
+      console.log(response.result);
+    }, function (reason) {
+      console.error('error: ' + reason.result.error.message);
+    });
+  } else {
+    clearInterval()
+    this.setState({loading:false})
+    this.setState({loadingComplete: true})
+  }
+  }.bind(this), 400)
+}.bind(this), 5000)
+  } 
+
+
+
+
+
+  handleOpenDialog = (e) => {
+    if (buttonRef.current) {
+      buttonRef.current.open(e)
+    }
+  }
+
+  handleOnFileLoad = (data) => {
+    console.log('---------------------------')
+    console.log(data)
+    console.log('---------------------------')
+    this.setState({datas:data})
+    this.setState({tableReady: true})
+  }
+
+  handleOnError = (err, file, inputElem, reason) => {
+    console.log(err)
+  }
+
+  handleOnRemoveFile = (data) => {
+    console.log('---------------------------')
+    console.log(data)
+    console.log('---------------------------')
+  }
+
+  handleRemoveFile = (e) => {
+    // Note that the ref is set async, so it might be null at some point
+    if (buttonRef.current) {
+      buttonRef.current.removeFile(e)
+    }
+  }
+
+  
+  
+  render() {
+    return (
+      <div style={{textAlign:'center'}}>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+         <>
+        <CSVReader
+          ref={buttonRef}
+          onFileLoad={this.handleOnFileLoad}
+          onError={this.handleOnError}
+          noClick
+          noDrag
+          onRemoveFile={this.handleOnRemoveFile}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {({ file }) => (
+            <aside style={{textAlign:'center'}}>
+              <Button
+                style={{marginBottom:10}}
+                color="primary"
+                variant="contained"
+                type='button'
+                onClick={this.handleOpenDialog}
+              >
+                Загрузить таблицу
+              </Button>
+              <div
+                style={{
+                  borderWidth: 1,
+                  borderStyle: 'solid',
+                  borderColor: '#ccc',
+                  height: 30,
+                  lineHeight: 2.5,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  marginRight: 10,
+                  marginLeft: 10,
+                  paddingLeft: 13,
+                  paddingTop: 3,
+                }}
+              >
+                {file && file.name}
+              </div>
+            </aside>
+          )}
+        </CSVReader>
+      </>
+      {this.state.loadingComplete ? 
+      <div>
+      <h2>Таблица готова</h2>
+      <h2>Опоздали:</h2>
+      <ul style={{listStyle:'none', marginLeft:-40}}>
+        {this.state.opozdun.map(item=>
+        <li>{item}</li>)}
+        </ul>
+      </div>: null
+  }
+      {this.state.loading ?
+      <div style={{marginTop: 50}}>
+        Заполняю таблицу...
+      </div> : null
+  }
+      {this.state.tableReady ?
+       <Button 
+       style={{marginTop:50}}
+       variant="contained" 
+       color="primary" 
+       onClick={this.onFormSubmit}>
+         ПОЕХАЛИ
+         </Button> : null
+  }
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,353 @@
+export const mainData = [
+    {   
+        id: 41,
+        name: 'Eugene Mardashov'
+    },
+    {   
+      id: 20,
+        name: 'Artyom Smirnov'
+    },
+    {
+      id: 88,
+        name: 'Yulia Dovgal'
+    },
+    {
+      id: 83,
+        name: 'Sergey Tovmasyan'
+    },
+    {
+      id: 68,
+        name: 'Nikolay Kostyurin'
+    },
+    {
+      id: 55,
+        name: 'Konstantin Kutsenko'
+    },
+    {
+      id: 28,
+        name: 'Виктор Селуков'
+    },
+    {
+      id: 87,
+        name: 'Elnur Ismailzada'
+    },
+    {
+      id: 44,
+       name: 'Evgeniya Dudareva'
+    },
+      {
+        id: 49,
+          name: 'elenakireeva'
+      },
+      {
+        id: 24,
+          name: 'Valentina Kuzmina'
+      },
+      {
+        id: 15,
+          name: 'Andrey Lebedevsky'
+      },
+      {
+        id: 10,
+          name: 'Alexey Savenko'
+      },
+      {
+        id: 18,
+          name: 'Anna Zueva'
+      },
+      {
+        id: 46,
+          name: 'Ekaterina Nazarova'
+      },
+      {
+        id: 23,
+          name: 'Bakhtiyar Alimbekov'
+      },
+      {
+        id: 30,
+          name: 'Vladimir Buyanov'
+      },
+      {
+        id: 61,
+          name: 'Mikhail Gusev'
+      },
+      {
+        id: 79,
+          name: 'Roman Pudashkin'
+      },
+      {
+        id: 11,
+          name: 'Alla Voropaeva'
+      },
+      {
+        id: 89,
+          name: 'Yulia Drozd'
+      },
+      {
+        id: 80,
+          name: 'Roman Yakovlev'
+      },
+      {
+        id: 71,
+          name: 'Olga Dosaeva'
+      },
+      {
+        id: 32,
+          name: 'vlad chuprin'
+      },
+      {
+        id: 50,
+          name: 'Janna Radovskaya'
+      },
+      {
+        id: 77,
+          name: 'Roman Kislenо́k'
+      },
+      {
+        id: 53,
+          name: 'Igor Korsukov'
+      },
+      {
+        id: 19,
+          name: 'Anton Chirtsov'
+      },
+      {
+        id: 85,
+          name: 'Stanislav Botev'
+      },
+      {
+        id: 5,
+          name: 'Alexander Ustinov'
+      },
+      {
+        id: 29,
+          name: 'Vlad'
+      },
+      {
+        id: 8,
+          name: 'Alexey Kovalevich'
+      },
+      {
+        id: 3,
+          name: 'Alexander Nazarov'
+      },
+      {
+        id: 47,
+          name: 'Ekaterina Smekhova'
+      },
+      {
+        id: 26,
+          name: 'Vasilij Pereverzev'
+      },
+      {
+        id: 4,
+          name: 'Alexandr Popov'
+      },
+      {
+        id: 56,
+          name: 'Kristina Zhukova'
+      },
+      {
+        id: 52,
+          name: 'Ivan Fedorov'
+      },
+      {
+        id: 65,
+          name: 'Nikita Zubarev'
+      },
+      {
+        id: 73,
+          name: 'Pavel Verkhovets'
+      },
+      {
+        id: 6,
+          name: 'Alexander Shcherban'
+      },
+      {
+        id: 2,
+          name: 'Alexander  Danchenko'
+      },
+      {
+        id: 66,
+          name: 'Nikita Korneev'
+      },
+      {
+        id: 69,
+          name: 'Nikolaj Moskovkin'
+      },
+      {
+        id: 59,
+          name: 'Maria Buryanina'
+      },
+      {
+        id: 34,
+          name: 'Давид Мандельштам'
+      },
+      {
+        id: 57,
+          name: 'Kristina L'
+      },
+      {
+        id: 60,
+          name: 'Maria Protasova'
+      },
+      {
+        id: 75,
+          name: 'Pavel Smokotnin'
+      },
+      {
+        id: 64,
+          name: 'Michael Trutnev'
+      },
+      {
+        id: 81,
+          name: 'Sergey Larin'
+      },
+      {
+        id: 14,
+          name: 'Andrew Kuzkou'
+      },
+      {
+        id: 38,
+          name: 'Dmitriy Shashkin'
+      },
+      {
+        id: 78,
+          name: 'Roman Pazilov'
+      },
+      {
+        id: 13,
+          name: 'Andrey Kovalev'
+      },
+      {
+        id: 76,
+          name: 'Paula Abdul'
+      },
+      {
+        id: 90,
+          name: 'Yulia Pototskaya'
+      },
+      {
+        id: 21,
+          name: 'Artem Sobolenkov'
+      },
+      {
+        id: 54,
+          name: 'Irina Tannik'
+      },
+      {
+        id: 33,
+          name: 'Vyacheslav Shalkevich'
+      },
+      {
+        id: 16,
+          name: 'Andrey Redkozubov'
+      },
+      {
+        id: 35,
+          name: 'Дмитрий Кретов'
+      },
+      {
+        id: 70,
+          name: 'Oleg Kashtalyan'
+      },
+      {
+        id: 12,
+          name: 'Andrei Golodok'
+      },
+      {
+        id: 22,
+          name: 'Artyom Feshchenko'
+      },
+      {
+        id: 72,
+          name: 'Olga Novikova'
+      },
+      {
+        id: 42,
+          name: 'Evgeniy Prokopiev'
+      },
+      {
+        id: 86,
+          name: 'Tatiana Naidenova'
+      },
+      {
+        id: 37,
+          name: 'Dmitry Maslennikov'
+      },
+      {
+        id: 91,
+          name: 'Yana Savchenko'
+      },
+      {
+        id: 17,
+          name: 'ADNREI IAKOBCHUK'
+      },
+      {
+        id: 74,
+          name: 'Pavel Pavlov'
+      },
+      {
+        id: 48,
+          name: 'Ekaterina Shmakova'
+      },
+      {
+        id: 7,
+          name: 'Sasha Smirnova'
+      },
+      {
+        id: 27,
+          name: 'Victor Kostyuk'
+      },
+      {
+        id: 67,
+          name: 'Nikolay Bulanov'
+      },
+      {
+        id: 43,
+          name: 'Eugene Yakshin'
+      },
+      {
+        id: 84,
+          name: 'Sofya Petrova'
+      },
+      {
+        id: 25,
+          name: 'Валерия Донич'
+      },
+      {
+        id: 10,
+          name: 'Aleksey Oliferchik'
+      },
+      {
+        id: 32,
+          name: 'Vladimir Skabelkin'
+      },
+      {
+        id: 37,
+          name: 'Dmitry Marchikhin'
+      },
+      {
+        id: 41,
+          name: 'Eugene Bondarev'
+      },
+      {
+        id: 59,
+          name: 'Maxim Chistyakov'
+      },
+      {
+        id: 64,
+          name: 'Mikhail Korolev'
+      },
+      {
+        id: 63,
+          name: 'Mikhail Kirilin'
+      },
+      {
+        id: 83,
+          name: 'Sergey Lysogor'
+      },
+  ]
+
+ 
+ 
