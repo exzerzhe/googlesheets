883bf7639f61540fa680722c929357ef4e1ad286
exzeshko <exzeshko@gmail.com>
2020-09-19 09:25:30 +0200

    игпы

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { mainData } from './data.js'
 import { Button } from '@material-ui/core';
 
 const buttonRef = React.createRef()
-const SPREADSHEET_ID = '12yEjI9815RFwUaJ8PpIIVgMztwcMYrLh9Ir5GbDoIIs'; //from the URL of your blank Google Sheet
-const CLIENT_ID = '457989737322-a2auu7j3br91c8iq7cqi0rbbg20qbiu4.apps.googleusercontent.com'; //from https://console.developers.google.com/apis/credentials
+const SPREADSHEET_ID = '1g1tvnn9WXl5bg2upXr7DBGXrgls_GmjSre2ODJqS6z0'; //from the URL of your blank Google Sheet
+const CLIENT_ID = '457989737322-kru3d1ph197vpsjo8k27d8snsk8rpj7s.apps.googleusercontent.com'; //from https://console.developers.google.com/apis/credentials
 const API_KEY = 'AIzaSyDX9HSVapiGgzfxo33QH-SMNqmU8CuIOII'; //https://console.developers.google.com/apis/credentials
 const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 
