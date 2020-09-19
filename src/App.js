238916dfaa2ea616324ea756f6861ed9ca1ed8c8
exzeshko <exzeshko@gmail.com>
2020-09-19 09:54:17 +0200

    bugs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Button } from '@material-ui/core';
 
 const buttonRef = React.createRef()
 const SPREADSHEET_ID = '1g1tvnn9WXl5bg2upXr7DBGXrgls_GmjSre2ODJqS6z0'; //from the URL of your blank Google Sheet
-const CLIENT_ID = '457989737322-kru3d1ph197vpsjo8k27d8snsk8rpj7s.apps.googleusercontent.com'; //from https://console.developers.google.com/apis/credentials
+const CLIENT_ID = '457989737322-joqnvmdh9ig7g0kah2keb4i7od6red57.apps.googleusercontent.com'; //from https://console.developers.google.com/apis/credentials
 const API_KEY = 'AIzaSyDX9HSVapiGgzfxo33QH-SMNqmU8CuIOII'; //https://console.developers.google.com/apis/credentials
 const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 
@@ -77,6 +77,18 @@ export default class ContactForm extends React.Component {
      }
    })
    this.setState({opozdun: opozdal})
+
+   var arr = []
+   let csvData = this.state.datas
+   mainData.forEach(function(item) {
+     csvData.forEach(function(items){
+       if(item.name === items.data[0]){
+           arr.push(item.id+1)
+       }
+     })
+   }
+   )
+
     let batchUpdateSpreadsheetRequestBody = {
       requests: [
         {
@@ -97,19 +109,11 @@ export default class ContactForm extends React.Component {
     request.then(function(response) {
       console.log(response.result);
     }, function(reason) {
-      console.error('error: ' + reason.result.error.message);
+      alert('Кажется ошибка: ' + reason.result.error.message);
+      arr = []
     });
   
-    let arr = []
-    let csvData = this.state.datas
-    mainData.forEach(function(item) {
-      csvData.forEach(function(items){
-        if(item.name === items.data[0]){
-            arr.push(item.id+1)
-        }
-      })
-    }
-    )
+  
     setTimeout(function(){
     let count = 0
     let i = -1
